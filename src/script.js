70d1283606dc4aa11c774b2d6a1a3e079a41b10d
masalkina-e <katyamasalkina123@yandex.ru>
2023-01-27 18:14:53 +0500

    Добавлена кнопка избранное

--- src/script.js
+++ src/script.js
@@ -52,12 +52,28 @@ function renderTours() {
 
             <div class="px-3 flex items-center justify-between">
                 <button class="btn-primary border border-solid border-sky-600 w-1/2 my-6">Подробнее</button>
-                <button><svg xmlns=http://www.w3.org/2000/svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-4 text-slate-500"><path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"/></svg></button>
+                <button id="btn-heart">
+                    <svg xmlns=http://www.w3.org/2000/svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-4 text-slate-500" id="empty-heart"><path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 mr-4 text-yellow-400 hidden" id="full-heart"><path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z"/></svg>
+                </button>
             </div>
 
-        </div>
+        </div>   
         ` 
     })
 }
+
+function clickHeart() {
+    const emptyHeart = document.getElementById('empty-heart')
+    const fullHeart = document.getElementById('full-heart')  
+
+    emptyHeart.classList.toggle('hidden')
+    fullHeart.classList.toggle('hidden')
+}
+
+const btnHeart = document.getElementById('btn-heart')
+btnHeart.addEventListener('click', clickHeart)
+
+
 
 renderTours()
