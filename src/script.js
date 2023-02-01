b31668423fda2d1feaf37561cddd8df35ad616b1
masalkina-e <katyamasalkina123@yandex.ru>
2023-02-01 18:48:07 +0500

    Отформатирована цена

--- src/script.js
+++ src/script.js
@@ -1,14 +1,18 @@
 import { format, differenceInDays  } from 'date-fns'
 
-async function loadTours() {
+async function renderTours() {
     const response = await fetch('https://www.bit-by-bit.ru/api/student-projects/tours')
     const data = await response.json()
 
     const boxTours = document.getElementById('box-tours')
     boxTours.innerHTML = ""
+
     data.forEach((tour) => {
 
         const duration = differenceInDays(new Date(tour.endTime), new Date(tour.startTime))
+
+        const currentFormatPrice = tour.price
+        const formatedPrice = currentFormatPrice.toLocaleString('ru-RU')+'.00'
 
         let location = `<span class="text-sm text-slate-600 font-normal text-justify">${tour.city}, ${tour.country}</span>`
 
@@ -29,7 +33,7 @@ async function loadTours() {
                 ${location}  
             </div>
             
-            <p class="pl-3 pr-7 text-sm font-semibold pt-4">от <span class="text-xl text-sky-600">${tour.price}</span> руб</p>
+            <p class="pl-3 pr-7 text-sm font-semibold pt-4">от <span class="text-xl text-sky-600">${formatedPrice}</span> руб</p>
 
             <div class="px-3 text-sm text-slate-600 mt-2 flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1"><path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" /></svg>          
@@ -53,5 +57,4 @@ async function loadTours() {
         ` 
     })
 }
-
-loadTours()
+renderTours()
