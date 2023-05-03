52c3607b9d9c41c7809c20bd9bc40e8ebb8ccaac
masalkina-e <katyamasalkina123@yandex.ru>
2023-05-03 15:21:54 +0500

    Доработка кода

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ import { format, differenceInDays  } from 'date-fns'
 let uniqCountry = []
 let favoriteTours = []
 let isFavoritePage = false
+let currentTourId
 
 async function loadTours() {
     const response = await fetch('https://www.bit-by-bit.ru/api/student-projects/tours')
@@ -92,7 +93,7 @@ function renderTours(tours) {
             </div>
 
             <div class="px-3 flex items-center justify-between">
-                <button class="btn-primary border border-solid border-sky-600 w-1/2 my-6">Подробнее</button>
+                <button id="btn-book-${tour.id}" class="btn-primary border border-solid border-sky-600 w-1/2 my-6">Забронировать</button>
                 <button id="btn-heart-${tour.id}">
                     ${iconEmptyHeart}
                     ${iconFullHeart}
@@ -111,6 +112,70 @@ function renderTours(tours) {
         
         btnHeart.addEventListener('click', makeAddToFavourites)
     }) 
+
+    tours.forEach((tour) => {
+        const btnBook = document.getElementById(`btn-book-${tour.id}`)
+
+        function makeOpenModalBook() {
+            openModalBook(tours, tour.id)
+        }
+        btnBook.addEventListener('click', makeOpenModalBook)
+    })
+}
+
+const modalWindow = document.getElementById('modal-window')
+const modalWindowForm = document.getElementById('modal-window-form')
+
+function openModalBook (tours, id) {
+
+    const tour = tours.find((tour) => {
+        return tour.id === id
+    })
+    currentTourId = tour.id
+    console.log(currentTourId)
+
+    const duration = differenceInDays(new Date(tour.endTime), new Date(tour.startTime))
+
+    const currentFormatPrice = tour.price
+    const formatedPrice = currentFormatPrice.toLocaleString('ru-RU')+'.00'
+
+    let location = `<span class="text-sm text-slate-600 font-normal text-justify">${tour.city}, ${tour.country}</span>`
+
+    if (tour.city == null ) {
+        location = `<span class="text-sm text-slate-600 font-normal text-justify">${tour.country}</span>`
+    }
+
+    document.getElementById('render-selected-tour').innerHTML = `
+    
+    <div class="bg-white rounded-t-xl flex justify-between">
+      
+        <div>
+            <div class="grow">
+                 <p class="text-lg font-bold">${tour.hotelName}</p>
+                 ${location}  
+            </div>
+        
+            <p class="text-sm font-semibold pt-4">от <span class="text-xl text-sky-600">${formatedPrice}</span> руб</p>
+
+            <div class="text-sm text-slate-600 mt-2 flex items-center">
+               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1"><path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" /></svg>          
+               <span class="pl-1">${format(new Date(tour.startTime),'dd.MM.yyyy')} - ${format(new Date(tour.endTime),'dd.MM.yyyy',)}</span>  
+            </div>
+
+            <div class="text-sm text-slate-600 mt-2 flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1"><path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                <span class="pl-1">${duration} дней</span>
+            </div> 
+        </div>
+        
+        <div>
+            <img class="h-40 w-full object-center object-cover rounded-xl" src=${tour.image} alt=""/>
+        </div>                
+    </div>
+    `
+    clearForm()
+    modalWindow.classList.toggle('hidden')
+    modalWindowForm.classList.toggle('hidden')
 }
 
 function toggleWindowFilterPrice() {
@@ -239,6 +304,85 @@ function saveToLocalStorage(tours) {
     localStorage.setItem('tours', toursJson)
 }
 
+async function bookTour() {   
+    const nameValue = document.getElementById("name").value
+    const phoneValue = document.getElementById("phone").value
+    const emailValue = document.getElementById("email").value
+    const discriptionValue = document.getElementById("discription").value
+
+    if (nameValue.length === 0 || phoneValue.length === 0 || emailValue.length === 0) {
+        alert("Необходимо заполнить все обязательные поля") 
+        return
+    } 
+
+    const params = {
+        customerName: nameValue,
+        phone: phoneValue,
+        email: emailValue, 
+        discription: discriptionValue 
+    }
+    console.log(params)
+
+    try {
+        const url = `https://www.bit-by-bit.ru/api/student-projects/tours/${currentTourId}`
+        console.log(currentTourId)
+        let response = await fetch(url, {
+           method: "POST",
+           body: JSON.stringify(params)
+        })
+        let data = await response.json()
+        // console.log(data)
+
+        modalWindowForm.classList.toggle('hidden')
+        document.getElementById('modal-window-message').classList.toggle('hidden')
+
+        document.getElementById('modal-window-message').innerHTML = `
+            <div class="flex flex-col items-center">
+                <p class="pt-5 text-slate-600 text-center">Запрос на бронирование тура успешно отправлен! В ближайщее время с Вами свяжется наш специалист.</p>
+                <button id="btn-done" class="btn-primary border border-solid border-sky-600 w-1/2 my-6">Готово</button>
+            </div>
+        `
+        
+        document.getElementById('btn-done').addEventListener('click', () => {
+            document.getElementById('modal-window-message').classList.toggle('hidden')
+            modalWindow.classList.toggle('hidden')
+        })
+
+    } catch (error) {
+        console.log (error)
+
+        modalWindowForm.classList.toggle('hidden')
+        document.getElementById('modal-window-message').classList.toggle('hidden')
+
+        document.getElementById('modal-window-message').innerHTML = `
+        <div class="flex flex-col items-center">
+            <div>
+                <img class="h-44 mt-8" src="/images/error.png" alt=""/>
+                <p class="pt-5 text-black text-center">Упс. Что-то пошло не так</p>
+            </div>
+            <button id="btn-exit" class="btn-primary border border-solid border-sky-600 w-1/2 my-6">Выйти</button>
+        </div>
+        `
+        
+        document.getElementById('btn-exit').addEventListener('click', () => {
+            document.getElementById('modal-window-message').classList.toggle("hidden")
+            modalWindow.classList.toggle('hidden')
+        })
+    } 
+}
+
+function closeModalWindow() {
+    modalWindow.classList.toggle('hidden')
+    modalWindowForm.classList.toggle('hidden')
+}
+
+function clearForm() {
+    document.getElementById("name").value = ""
+    document.getElementById("phone").value = ""
+    document.getElementById("email").value = ""
+    document.getElementById("discription").value = ""
+}
+
 let tours 
 const boxLoader = document.getElementById('box-loader')
 
@@ -249,11 +393,15 @@ if (!tours) {
 }
 
 async function init() {
-    tours = await loadTours()
+    const tours = await loadTours()
 
     if (tours) {
         renderTours(tours)
         boxLoader.innerHTML = ""
+    }
+
+    async function makeBookTour() {
+        await bookTour()
     }
     
     renderUniqCountry(tours)
@@ -267,6 +415,8 @@ async function init() {
     document.getElementById("btn-close-min-max-price").addEventListener('click',toggleWindowFilterPrice)
     document.getElementById("btn-close-min-max-price").addEventListener('click', () => filterPrice(tours))
     document.getElementById('btn-favorite').addEventListener('click', () => filterToFavorite(tours))
+    document.getElementById('btn-send').addEventListener('click', makeBookTour)
+    document.getElementById('btn-close-mw').addEventListener('click', closeModalWindow)
 }
 
 init()
